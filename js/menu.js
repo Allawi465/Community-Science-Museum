c4f42c01a148c1cc4511f34b1bed251925e9bbd7
Allawi465 <allawi465@gmail.com>
2022-03-16 02:45:59 +0100

    .

--- js/menu.js
+++ js/menu.js
@@ -12,10 +12,9 @@ const navDrag = () => {
             if (link.style.animation) {
                 link.style.animation = "";
             } else {
-                link.style.animation = `navLinkOpacity 0.5s ease forwards ${index / 7 + 0.2}s`;
+                link.style.animation = `navLinkOpacity 0.5s ease forwards ${index / 7 + 0.1}s`;
             }
         });
-        hOne.classList.toggle("h1Display");
     })
 };
 
